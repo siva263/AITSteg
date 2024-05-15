fc37c7e57105a7e36dc43ce659882f4f61752215
siva263 <130476034+siva263@users.noreply.github.com>
2024-05-15 17:46:14 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,27 +1,21 @@
 import sys
+sys.path
+
 from embed import embedFunc
 from extract import extractFunc
-from AES import encrypt, decrypt
-import pygetwindow as gw
-
+from AES import encrypt,decrypt
+import pyperclip
 # SM=input("Enter secret message:")
 # CM=input("Enter cover message:")
 # password=input("Enter password for encryption:")
 
-def copy_to_clipboard(text):
-    window = gw.getWindowsWithTitle('Your Window Title')[0]  # Adjust window title as needed
-    window.copyToClipboard(text)
-
-def hideFunc(SM, password, CM):
-    encSM = encrypt(password, SM)
-    print("Encrypted secret message going to send:", encSM)
-    CM_HM = embedFunc(encSM, CM)
-    print("Cover message=", CM_HM)
-    copy_to_clipboard(CM_HM)
+def hideFunc(SM,password,CM):
+    encSM=encrypt(password,SM)
+    print("Encrypted secret message going to send:",encSM)
+    CM_HM=embedFunc(encSM,CM)
+    print("Cover message=",CM_HM)
+    pyperclip.copyToClipboard(CM_HM)
     return CM_HM
+    
 
-# Example usage:
-# SM = input("Enter secret message:")
-# CM = input("Enter cover message:")
-# password = input("Enter password for encryption:")
-# hideFunc(SM, password, CM)
+#print("copy this message and paste it to send")
