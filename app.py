76c5c4116aadf6250af8a53de8e382dadd977337
siva263 <130476034+siva263@users.noreply.github.com>
2025-02-25 01:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,session
 from flask import render_template,url_for
 
 from sender import hideFunc
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    session.pop('result_reveal',None)
     return render_template("homepage.html")
 
 @app.route("/hide",methods=['POST','GET'])
